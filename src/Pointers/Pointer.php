b2611c4bf1ab907c0d6792ba0632bc1dadf16528
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2022-09-20 18:08:37 +0200

    Implement pointer

--- src/Pointers/Pointer.php
+++ src/Pointers/Pointer.php
@@ -2,6 +2,7 @@
 
 namespace Cerbero\JsonParser\Pointers;
 
+use Cerbero\JsonParser\Tree;
 use Stringable;
 
 /**
@@ -11,12 +12,106 @@ use Stringable;
 class Pointer implements Stringable
 {
     /**
+     * The reference tokens.
+     *
+     * @var string[]
+     */
+    protected array $referenceTokens;
+
+    /**
+     * The pointer depth.
+     *
+     * @var int
+     */
+    protected int $depth;
+
+    /**
      * Instantiate the class.
      *
      * @param string $pointer
      */
     public function __construct(protected string $pointer)
     {
+        $this->referenceTokens = $this->toReferenceTokens();
+        $this->depth = count($this->referenceTokens);
+    }
+
+    /**
+     * Turn the JSON pointer into reference tokens
+     *
+     * @return array
+     */
+    protected function toReferenceTokens(): array
+    {
+        $tokens = explode('/', substr($this->pointer, 1));
+
+        return array_map(fn (string $token) => str_replace(['~1', '~0'], ['/', '~'], $token), $tokens);
+    }
+
+    /**
+     * Retrieve the reference tokens
+     *
+     * @return array
+     */
+    public function referenceTokens(): array
+    {
+        return $this->referenceTokens;
+    }
+
+    /**
+     * Retrieve the JSON pointer depth
+     *
+     * @return int
+     */
+    public function depth(): int
+    {
+        return $this->depth;
+    }
+
+    /**
+     * Determine whether the reference token at the given depth matches the provided node
+     *
+     * @param int $depth
+     * @param mixed $node
+     * @return bool
+     */
+    public function depthMatchesNode(int $depth, mixed $node): bool
+    {
+        if (!isset($this->referenceTokens[$depth])) {
+            return false;
+        }
+
+        if ($this->referenceTokens[$depth] === (string) $node) {
+            return true;
+        }
+
+        return is_int($node) && $this->referenceTokens[$depth] === '-';
+    }
+
+    /**
+     * Determine whether the pointer matches the given tree
+     *
+     * @param Tree $tree
+     * @return bool
+     */
+    public function matchesTree(Tree $tree): bool
+    {
+        return $this->referenceTokens == $tree->original() || $this->referenceTokens == $tree->wildcarded();
+    }
+
+    /**
+     * Determine whether the pointer includes the given tree
+     *
+     * @param Tree $tree
+     * @return bool
+     */
+    public function includesTree(Tree $tree): bool
+    {
+        if (($firstNest = array_search('-', $this->referenceTokens)) === false) {
+            return false;
+        }
+
+        return array_slice($this->referenceTokens, 0, $firstNest) == array_slice($tree->original(), 0, $firstNest);
     }
 
     /**
