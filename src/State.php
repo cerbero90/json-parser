f6fb63fa7ce105ba30eb9705c6737436bb99a11f
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2022-09-20 18:08:21 +0200

    Implement state

--- src/State.php
+++ src/State.php
@@ -8,5 +8,33 @@ namespace Cerbero\JsonParser;
  */
 class State
 {
-    //
+    /**
+     * The JSON tree.
+     *
+     * @var Tree
+     */
+    public Tree $tree;
+
+    /**
+     * Whether the tree changed.
+     *
+     * @var bool
+     */
+    public bool $treeChanged = false;
+
+    /**
+     * The JSON buffer.
+     *
+     * @var string
+     */
+    public string $buffer = '';
+
+    /**
+     * Instantiate the class.
+     *
+     */
+    public function __construct()
+    {
+        $this->tree = new Tree();
+    }
 }
