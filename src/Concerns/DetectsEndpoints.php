820b485113663742ac0e05cf66a92a0445eb420d
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2022-10-29 17:02:41 +1000

    Implement trait to detect endpoints

---
+++ src/Concerns/DetectsEndpoints.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cerbero\JsonParser\Concerns;
+
+/**
+ * The trait to detect endpoints.
+ *
+ */
+trait DetectsEndpoints
+{
+    /**
+     * Determine whether the given string points to an endpoint
+     *
+     * @param string $string
+     * @return bool
+     */
+    public function isEndpoint(string $string): bool
+    {
+        if (($url = parse_url($string)) === false) {
+            return false;
+        }
+
+        return in_array($url['scheme'] ?? null, ['http', 'https']) && isset($url['host']);
+    }
+}
