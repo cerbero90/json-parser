1d7d65b0513be7da42ef8b76610e95db323dcfc2
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2022-12-03 13:34:13 +1000

    Improve expectations

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,30 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
-|
-*/
-
-// uses(Tests\TestCase::class)->in('Feature');
-
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
+/**
+ * Expect that keys and values are parsed correctly
+ *
+ * @param array $expected
+ * @return Expectation
+ */
 expect()->extend('toParseTo', function (array $expected) {
+    $actual = [];
+
+    foreach ($this->value as $parsedKey => $parsedValue) {
+        expect($parsedValue)->toBe($expected[$parsedKey]);
+
+        $actual[$parsedKey] = $parsedValue;
+    }
+
+    return expect($actual)->toBe($expected);
+});
+
+/**
+ * Expect that values defined by JSON pointers are parsed correctly
+ *
+ * @param array $expected
+ * @return Expectation
+ */
+expect()->extend('toPointTo', function (array $expected) {
     $actual = $itemsCount = [];
 
     foreach ($this->value as $parsedKey => $parsedValue) {
@@ -39,18 +39,5 @@ expect()->extend('toParseTo', function (array $expected) {
         };
     }
 
-    expect($actual)->toBe($expected);
+    return expect($actual)->toBe($expected);
 });
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-// ...
