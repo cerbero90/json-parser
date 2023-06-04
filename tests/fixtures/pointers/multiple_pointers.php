263de30577296e49a4ba457b0c3c58e5b03634fc
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2023-06-04 17:24:20 +0200

    Add fixtures

--- tests/fixtures/pointers/multiple_pointers.php
+++ tests/fixtures/pointers/multiple_pointers.php
@@ -78,6 +78,56 @@ return [
                 "type" => "Devil's Food",
             ],
         ],
+        '/batters/batter,/topping' => [
+            'batter' => [
+                [
+                    "id" => "1001",
+                    "type" => "Regular",
+                ],
+                [
+                    "id" => "1002",
+                    "type" => "Chocolate",
+                ],
+                [
+                    "id" => "1003",
+                    "type" => "Blueberry",
+                ],
+                [
+                    "id" => "1004",
+                    "type" => "Devil's Food",
+                ],
+            ],
+            'topping' => [
+                [
+                    "id" => "5001",
+                    "type" => "None",
+                ],
+                [
+                    "id" => "5002",
+                    "type" => "Glazed",
+                ],
+                [
+                    "id" => "5005",
+                    "type" => "Sugar",
+                ],
+                [
+                    "id" => "5007",
+                    "type" => "Powdered Sugar",
+                ],
+                [
+                    "id" => "5006",
+                    "type" => "Chocolate with Sprinkles",
+                ],
+                [
+                    "id" => "5003",
+                    "type" => "Chocolate",
+                ],
+                [
+                    "id" => "5004",
+                    "type" => "Maple",
+                ],
+            ],
+        ],
     ],
     'empty_array' => [
         '/-1,/-2' => [],
