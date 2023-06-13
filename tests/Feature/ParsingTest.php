840469a24f7887c5c56e283b3682e4abfd008899
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2023-06-13 22:13:58 +0200

    Add parsing tests

--- tests/Feature/ParsingTest.php
+++ tests/Feature/ParsingTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Cerbero\JsonParser\Dataset;
+use Cerbero\JsonParser\Decoders\SimdjsonDecoder;
 use Cerbero\JsonParser\JsonParser;
 
 use function Cerbero\JsonParser\parseJson;
@@ -16,6 +17,14 @@ it('parses JSON when instantiated statically', function (string $json, array $pa
 
 it('parses JSON when calling the helper', function (string $json, array $parsed) {
     expect(parseJson($json))->toParseTo($parsed);
+})->with(Dataset::forParsing());
+
+it('parses with custom decoders', function (string $json, array $parsed) {
+    expect(JsonParser::parse($json)->decoder(new SimdjsonDecoder()))->toParseTo($parsed);
+})->with(Dataset::forParsing());
+
+it('parses a custom number of bytes', function (string $json, array $parsed) {
+    expect(JsonParser::parse($json)->bytes(1024))->toParseTo($parsed);
 })->with(Dataset::forParsing());
 
 it('eager loads JSON into an array', function (string $json, array $parsed) {
